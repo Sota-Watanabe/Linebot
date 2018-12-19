f850751bc1e77ad16445f1ab8c0ba5a87c836a2b
Sota222 <cy16179@shibaura-it.ac.jp>
2018-12-19 18:52:33 +0900

    change stamp 16

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def handle_sticker(event):
         (
             StickerSendMessage(
                 package_id='1',
-                sticker_id='5'
+                sticker_id='6'
             ),
         ))
         #スタンプにはpackage_idとsticker_idが存在する
